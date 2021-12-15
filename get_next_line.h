cbb65485505fba5430ac6376f4d44ad43ac56a57
Luernios <82155421+Luernios@users.noreply.github.com>
2021-12-15 18:09:22 +0100

    first push

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrigaudy <vrigaudy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 17:41:33 by vrigaudy          #+#    #+#             */
+/*   Updated: 2021/12/15 18:06:35 by vrigaudy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
