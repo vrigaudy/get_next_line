5176396734bc1f61b59c89fe9196050d4c28af7e
Victor RIGAUDY <vrigaudy@e1r7p12.42.fr>
2021-12-21 12:52:48 +0100

    j'ai faim

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vrigaudy <vrigaudy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 17:41:33 by vrigaudy          #+#    #+#             */
-/*   Updated: 2021/12/15 18:06:35 by vrigaudy         ###   ########.fr       */
+/*   Updated: 2021/12/21 10:56:19 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
+
+int		ft_strlen(char *str);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,31 @@
 /*   By: vrigaudy <vrigaudy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 17:42:02 by vrigaudy          #+#    #+#             */
-/*   Updated: 2021/12/15 18:04:33 by vrigaudy         ###   ########.fr       */
+/*   Updated: 2021/12/21 12:52:05 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strchr(char *str, char c)
+{
+	while (*str )
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin()
